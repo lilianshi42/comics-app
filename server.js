32865f78b1533a997f34f446acb2ded9e0256aa4
SaltedEggYolk <50718765+rockerlilian@users.noreply.github.com>
2021-06-12 14:25:41 -0400

    server.js modified

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const path = require("path");
 const DataService = require("./data-service");
 
 app.use(cors());
@@ -9,15 +8,15 @@ app.use(cors());
 // Add support for incoming JSON entities
 app.use(express.json());
 
-// CORS in ExpressJS
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// // CORS in ExpressJS
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.get("/api", cors(), (req, res) => {
   const url = "https://xkcd.com/info.0.json";
